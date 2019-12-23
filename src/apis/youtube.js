001892eb5bc59fdf5b38ab159bd27a5a9dc5442c
chrstphgrnr <christophgruener@me.com>
2019-12-22 21:31:31 -0700

    add axios to project

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,11 @@
+import axios from 'axios'
 const KEY = 'AIzaSyCN6JlNjZAtpQ23Ve9pdjD3_eupNeLYxCY'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3', 
+    params: {
+        part: 'snippet', 
+        maxResults: 5, 
+        key: KEY
+    }
+})
